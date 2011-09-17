09bc98686ba310733cb91e04eb5e855370d7fe24
a.lementuev@gmail.com <a.lementuev@gmail.com@a1224c3d-cd34-58c7-e1d0-e48322bb8bca>
2011-09-17 04:01:15 +0000

    Remove printing

--- trunk/src/flexprettyprint/handlers/ActionScriptFormatter.java
+++ trunk/src/flexprettyprint/handlers/ActionScriptFormatter.java
@@ -1,9 +1,5 @@
 package flexprettyprint.handlers;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,38 +7,6 @@ import actionscriptinfocollector.AntlrUtilities;
 
 public class ActionScriptFormatter {
 
-	/**
-	 * @param args
-	 * @throws IOException 
-	 */
-	public static void main(String[] args) throws IOException
-	{
-		File f = new File(args[0]);
-		BufferedReader br=new BufferedReader(new FileReader(f));
-		StringBuffer buffer=new StringBuffer();
-		while (true)
-		{
-			String d=br.readLine();
-			if (d==null)
-				break;
-			buffer.append(d);
-			buffer.append("\n");
-		}
-		String data=buffer.toString();
-		try
-		{
-			String result=null;
-			result=new ASPrettyPrinter(true, data).print(0);
-			System.out.println("*****************************************************");
-			System.out.println(result);
-		}
-		catch (Exception e)
-		{
-			System.out.println("Error pretty printing");
-			e.printStackTrace();
-		}
-	}
-	
 	public static int getNumberOfEmptyLinesAtEnd(StringBuffer buffer)
 	{
 		return getNumberOfEmptyLinesAtEnd(buffer, -1);
