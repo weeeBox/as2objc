375bdc6cd030850981ea2180a104236d81ecf52d
a.lementuev@gmail.com <a.lementuev@gmail.com@a1224c3d-cd34-58c7-e1d0-e48322bb8bca>
2011-09-18 22:47:33 +0000

    Progress

--- src/block/processors/VarLineParser.java
+++ src/block/processors/VarLineParser.java
@@ -3,6 +3,8 @@ package block.processors;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import as2ObjC.CodeHelper;
+
 public class VarLineParser extends LineProcessor
 {
 	private Pattern pattern = Pattern.compile("var" + SPACE + IDENTIFIER + MBSPACE + ":" + MBSPACE + IDENTIFIER);
@@ -13,7 +15,7 @@ public class VarLineParser extends LineProcessor
 		Matcher m = pattern.matcher(line);
 		if (m.find())
 		{
-			return m.replaceFirst(m.group(2) + " " + m.group(1));
+			return m.replaceFirst(CodeHelper.type(m.group(2)) + " " + m.group(1));
 		}
 		
 		return line;
