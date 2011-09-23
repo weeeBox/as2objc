974b9b00d584cb893d0f531eea461a093288407a
a.lementuev@gmail.com <a.lementuev@gmail.com@a1224c3d-cd34-58c7-e1d0-e48322bb8bca>
2011-09-23 01:24:11 +0000

    Progress

--- trunk/src/block/processors/DotLineProcessor.java
+++ trunk/src/block/processors/DotLineProcessor.java
@@ -7,7 +7,9 @@ import static block.RegexHelp.LPAR;
 import static block.RegexHelp.MBSPACE;
 import static block.RegexHelp.RPAR;
 import static block.RegexHelp.SPACE;
+
 import static block.RegexHelp.mb;
+import static block.RegexHelp.group;
 
 import java.util.List;
 import java.util.regex.Matcher;
@@ -19,77 +21,77 @@ public class DotLineProcessor extends LineProcessor
 {
 	private static final String MBNEW = mb("new" + SPACE);
 	
-	private Pattern pattern = Pattern.compile(mb(DOT) + MBNEW + IDENTIFIER + MBSPACE + LPAR + ANY + RPAR);
+	private Pattern pattern = Pattern.compile(group(mb(DOT) + MBNEW + IDENTIFIER + MBSPACE + LPAR));
 
-	private static final int GR_DOT = 1;
-	private static final int GR_NEW = 2;
-	private static final int GR_IDENTIFIER = 3;
-	private static final int GR_ARGS = 4;
+	private static final int GR_DOT = 2;
+	private static final int GR_NEW = 3;
+	private static final int GR_IDENTIFIER = 4;
 
 	private static final String separators = "()[]\b\f\t\n\r|&^+-*/!%=~\\?<>:;{}&@#`\"";
 	
 	@Override
 	public String process(String line)
 	{
-		String temp = line;
 		
-		Matcher matcher = pattern.matcher(temp); 
-		if (matcher.find())
+		Matcher matcher = pattern.matcher(line); 
+		while (matcher.find())
 		{
-			line = processLine(line, matcher);
+			String identifier = matcher.group(GR_IDENTIFIER);
+			if (CodeHelper.isFlowOperator(identifier))
+			{
+				continue;
+			}
+
+			boolean isConstructor = matcher.group(GR_NEW) != null;
+			boolean hasCallTarget = matcher.group(GR_DOT) != null;
+			
+			String argsStr = LineProcHelp.parenthesisVal(line, matcher.end() - 1);
+			System.out.println(matcher.group(1) + argsStr + ")");
+			
+			StringBuilder argsBuf = new StringBuilder();
+			if (argsStr.length() > 0)
+			{
+				List<String> args = LineProcHelp.splitArgs(argsStr, ',');
+				int argIndex = 0;
+				for (String arg : args)
+				{
+					argsBuf.append(":");
+					argsBuf.append(arg);
+					if (++argIndex < args.size())
+					{
+						argsBuf.append(" ");
+					}
+				}
+			}
+			
+			String oldCode;
+			String newCode;
+			
+			if (isConstructor)
+			{
+				oldCode = line.substring(matcher.start(), matcher.end() + argsStr.length() + 1);
+				newCode = createCall("[" + identifier + " alloc]", "init", argsBuf.toString());
+			}
+			else if (hasCallTarget)
+			{
+				int targetStart = findTargetStart(line, matcher.start() - 1);
+				String target = line.substring(targetStart, matcher.start()).trim();
+				
+				newCode = createCall(target, identifier, argsBuf.toString());
+				oldCode = line.substring(targetStart, matcher.end() + argsStr.length() + 1);
+			}
+			else
+			{
+				newCode = createCall("self", identifier, argsBuf.toString());
+				oldCode = line.substring(matcher.start(), matcher.end() + argsStr.length() + 1);
+			}
+			line = line.replace(oldCode, newCode);
+			matcher = pattern.matcher(line);
 		}
 		
 		return line;
 	}
-
-	private String processLine(String line, Matcher matcher)
-	{
-		String identifier = matcher.group(GR_IDENTIFIER);
-		if (CodeHelper.isFlowOperator(identifier))
-		{
-			return line;
-		}
-
-		boolean isConstructor = matcher.group(GR_NEW) != null;
-		boolean hasCallTarget = matcher.group(GR_DOT) != null;
-		
-		String argsStr = LineProcHelp.parenthesisVal("(" + matcher.group(GR_ARGS) + ")", 0);
-		StringBuilder argsBuf = new StringBuilder();
-		if (argsStr.length() > 0)
-		{
-			List<String> args = LineProcHelp.splitArgs(argsStr, ',');
-			int argIndex = 0;
-			for (String arg : args)
-			{
-				argsBuf.append(":");
-				argsBuf.append(arg);
-				if (++argIndex < args.size())
-				{
-					argsBuf.append(" ");
-				}
-			}
-		}
-		
-		if (isConstructor)
-		{
-			return matcher.replaceAll(createCall("[" + identifier + " alloc]", "init", argsBuf.toString()));
-		}
-		else if (hasCallTarget)
-		{
-			int targetStart = findTargetStart(line, matcher.start() - 1);
-			String target = line.substring(targetStart, matcher.start()).trim();
-			
-			String newCode = createCall(target, identifier, argsBuf.toString());
-			String oldCode = line.substring(targetStart, matcher.end());
-			
-			return line.replace(oldCode, newCode);
-		}
-		else
-		{
-			return matcher.replaceAll(createCall("self", identifier, argsBuf.toString()));
-		}
-	}
-
+	
 	private String createCall(String target, String message, String args)
 	{
 		String newCode = "[" + target + " " + message + args + "]";
