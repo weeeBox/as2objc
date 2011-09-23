14bb115d811362018d308170a09a88fd8ab76b1c
a.lementuev@gmail.com <a.lementuev@gmail.com@a1224c3d-cd34-58c7-e1d0-e48322bb8bca>
2011-09-23 00:49:36 +0000

    Fucking progress

--- trunk/src/block/processors/LineProcessor.java
+++ trunk/src/block/processors/LineProcessor.java
@@ -4,7 +4,5 @@ import block.RegexHelp;
 
 public abstract class LineProcessor
 {
-	protected static final String group(String line) { return RegexHelp.group(line); }
-	
 	public abstract String process(String line);
 }
