b89895e03a983a00baebf87cbb6f7064f1acb491
a.lementuev <a.lementuev@a1224c3d-cd34-58c7-e1d0-e48322bb8bca>
2011-09-27 18:35:06 +0000

    Fixes

--- src/block/processors/DoubleToFloat.java
+++ src/block/processors/DoubleToFloat.java
@@ -1,13 +1,14 @@
 package block.processors;
 
 import static block.RegexHelp.DOT;
+import static block.RegexHelp.group;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DoubleToFloat extends LineProcessor
 {
-	private static Pattern pattern = Pattern.compile("[\\-+\\d]+" + DOT + "[\\d]+");
+	private static Pattern pattern = Pattern.compile(group("[\\-+\\d]+" + DOT + "[\\d]+") + "[^fF]");
 	
 	@Override
 	public String process(String line)
@@ -17,7 +18,7 @@ public class DoubleToFloat extends LineProcessor
 		Matcher matcher = pattern.matcher(line);
 		while (matcher.find())
 		{
-			String number = matcher.group();
+			String number = matcher.group(1);
 			temp = temp.replace(number, number + "f");
 		}
 		return temp;
